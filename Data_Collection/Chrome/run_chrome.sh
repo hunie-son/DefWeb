1600f88d7e826fb688310dd3ce4bf2a2c4aa5b63
hunie <37320810+hunie-son@users.noreply.github.com>
2023-08-22 11:08:21 -0500

    Update run_chrome.sh

--- Data_Collection/Chrome/run_chrome.sh
+++ Data_Collection/Chrome/run_chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Java Script code based on the Website List
+#JavaScript code based on the Website List
 for ((j=1;j<=100;j++))
 do
     echo "Start Collecting Website $j"
@@ -8,6 +8,7 @@ do
     for ((i=1;i<=100;i++))
     do
         echo "Website: $j Mesurement: $i"
+        #Run Browser with JavaScript Code
         google-chrome PrimeProbe_Test_"$j".html &
         #base on the attack occurs 30s
         sleep 35s
