8914be7e9317f919833074e19999a4a5022d25f4
hunie <37320810+hunie-son@users.noreply.github.com>
2023-08-27 21:20:34 -0500

    Create run_demo.sh

---
+++ Data_Collection/run_demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#JavaScript code based on the Website List
+for ((i=1;i<=100;i++))
+do
+    echo "Website: $j Mesurement: $i"
+    #Run Browser(Google Chrome) with JavaScript Code (amazon.com)
+    #Change to "PrimeProbe_google.html" to achieve google.com WF data
+    google-chrome PrimeProbe_amazon.html &
+        
+    #base on the attack occurs 30s
+    sleep 35s
+    killall chrome
+    sleep 10s
+done
