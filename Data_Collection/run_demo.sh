dfc75cdc1f59f5c87ba64e249e06d761b2a2a010
hunie <37320810+hunie-son@users.noreply.github.com>
2023-08-28 18:09:08 -0500

    Update run_demo.sh

--- Data_Collection/run_demo.sh
+++ Data_Collection/run_demo.sh
@@ -13,3 +13,16 @@ do
     killall chrome
     sleep 10s
 done
+
+for ((i=1;i<=100;i++))
+do
+    echo "Website: $j Mesurement: $i"
+    #Run Browser(Google Chrome) with JavaScript Code (amazon.com)
+    #Change to "PrimeProbe_google.html" to achieve google.com WF data
+    google-chrome PrimeProbe_google.html &
+        
+    #base on the attack occurs 30s
+    sleep 35s
+    killall chrome
+    sleep 10s
+done
